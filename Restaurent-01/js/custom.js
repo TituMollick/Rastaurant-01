c8aa75fafe028a3ef2846ed4e16647d83d2d077c
TituMollick <117148051+TituMollick@users.noreply.github.com>
2022-11-06 22:21:54 +0600

    Add files via upload

---
+++ Restaurent-01/js/custom.js
@@ -0,0 +1,14 @@
+
+  // Titu Mollick
+	// titu99125@gmail.com 
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+    
+  })(window.jQuery);
